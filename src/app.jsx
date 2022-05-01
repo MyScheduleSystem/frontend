ec27348c34bb5affe3b23390596e457df518133f
vpvm96 <vpvm96@gmail.com>
2022-05-01 14:20:12 +0900

    파일 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import styles from "./app.module.css";
 import Login from "./components/login/login";
-import Calendar from "./components/calendar/calendar";
 
 function App() {
   return (
     <div className={styles.body}>
-      <Routes>
+      {/* <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/calendar/*" element={<Calendar />} />
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
