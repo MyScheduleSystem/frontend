7c84442547e4e3942646c727f5aea700fd03f6f3
foxmon <jjockrod@naver.com>
2022-05-07 21:38:46 +0900

    Data스펙 변경 및 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import styles from "./app.module.css";
-import TestData from "./dev/testData";
 
 function App() {
-    const testData2 = getInitData.call(this);
     return (
         <div className={styles.body}>
-            {testData2.title}
-            <div>{testData2.contents.map((el) => el.description)}</div>
         </div>
     );
 }
 
-function getInitData() {
-    return TestData.container.calender.todo;
-}
 
 export default App;
