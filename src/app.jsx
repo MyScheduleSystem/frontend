8c4e800f30777cd99c3dcf341ae6b14cdecc757c
vpvm96 <vpvm96@gmail.com>
2022-05-09 15:14:44 +0900

    수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./app.module.css";
-import SideBar from "./components/sidebar/sidebar"
+import SideBar from "./components/sidebar/sidebar";
+import Modal from "./components/page/modal";
 
 function App() {
     return (
         <div className={styles.body}>
-            <SideBar></SideBar>
+            {/* <SideBar></SideBar> */}
+            <Modal></Modal>
         </div>
     );
 }
-
 
 export default App;
 ///asdfsdafsdafdsfs
