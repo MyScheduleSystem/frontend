67f30c9cb0a28c62deb42ab7da1b15fadb08bd7b
vpvm96 <vpvm96@gmail.com>
2022-05-10 23:49:12 +0900

    index.js import순서 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './app';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
