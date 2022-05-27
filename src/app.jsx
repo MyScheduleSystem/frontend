8b4bd128d5be35ab62d7d3146bcf724e8eabfd77
vpvm96 <vpvm96@gmail.com>
2022-05-27 16:29:39 +0900

    수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import { Route, Routes } from "react-router-dom";
-import SideBar from "./components/sidebar/sidebar";
-import MyCalendar from "./components/calendar/myCalendar";
-import MyCard from "./components/card/myCard";
-=======
 import MyRouter from "./pages/router/myRouter";
-// Modal Update, Delete 추가 - leo
->>>>>>> Stashed changes
-=======
-import MyRouter from "./pages/router/myRouter";
-
-// Modal Update, Delete 추가 - leo
->>>>>>> 59c8a1a004229b16538ba7272d826bc79d9d3729
 
 function App() {
     return (
