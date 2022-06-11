1d85330c84e25d2fbeffe251ecbf91a1b25abcd8
foxmon <jjockrod@naver.com>
2022-06-11 22:44:38 +0900

    Refactor:userFetcher

--- src/fetcher/userFetcher.js
+++ src/fetcher/userFetcher.js
@@ -1,26 +1,48 @@
-import axios from 'axios'
-import ErrorUtil from '../util/errorUtil'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import firestore from '../service/firebase'
+import User from '../type/user'
 
-const baseUrl = process.env.REACT_APP_BASE_URL
 const userFetcher = {}
 
 userFetcher.signin = async function(user) {
-    ErrorUtil.invalidParameter(user)
-    const { username, password } = user
-    const { data } = await axios.post(`${baseUrl}/user/login`, { username, password })
-    if(!data) return
-    return data
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+            const userObj = userCredential.user
+            const uuid = userObj.uid
+            const token = userObj.getIdToken()
+            const createdUser = User.createStorage(uuid, token)
+            createdUser.save()
+        })
+        .catch(error => console.error(error))
 }
 
-userFetcher.signup = async(user) => {
-    ErrorUtil.invalidParameter(user)
-    const { username, password, name, email } = user
-    const { data } = await axios.post(`${baseUrl}/user/signup`, { username, password, name, email })
-    if(!data) return
-    return data
+userFetcher.signup = async (user) => {
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+            const userObj = userCredential.user
+            createUserDocument(userObj)
+        })
+        .catch(error => console.error(error))
 }
 
-// TODO: signup
+userFetcher.signout = () => {
+    const auth = getAuth()
+    signOut(auth)
+        .then(() => User.clearStorage())
+        .catch(error => console.error(error))
+}
+
+async function createUserDocument(userObj) {
+    await setDoc(doc(firestore, "users", userObj), {
+        usernmae: userObj.username,
+        name: userObj.name,
+        email: userObj.email,
+        password: userObj.password,
+    })
+}
 
 Object.freeze(userFetcher)
 export default userFetcher
