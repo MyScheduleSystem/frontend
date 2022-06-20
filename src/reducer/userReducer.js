9654ad7fdb9bec6c8bbef3f6ec6da5d55cf8b573
foxmon <jjockrod@naver.com>
2022-06-20 13:46:44 +0900

    Refactor: userReducer signin부분 수정

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -11,6 +11,8 @@ UserInitialState.type = "userInitialState"
 UserInitialState.refreshToken = null
 UserInitialState.accessToken = null
 UserInitialState.authenticated = false
+UserInitialState.fetchOption = {}
+UserInitialState.fetchOption.uuid = null
 
 UserReducer.clearUserState = function() {
     UserInitialState.refreshToken = null
@@ -24,21 +26,27 @@ UserReducer.clearUserState = function() {
 UserReducer.userReducer = function(state, action) {
     switch(action.type) {
         case UserActionType.type.signup:
-            ErrorUtil.invalidParameter(action.result)
             return {
                 authenticated: action.result,
             }
 
-        case UserActionType.type.signin:
-            ErrorUtil.invalidParameter(action.accessToken)
-            ErrorUtil.invalidParameter(action.refreshToken)
-            ErrorUtil.invalidParameter(action.result)
+        case UserActionType.type.signin: {
+            const obj = action.action
+            ErrorUtil.invalidParameter(obj.accessToken)
+            ErrorUtil.invalidParameter(obj.refreshToken)
+            ErrorUtil.invalidParameter(obj.fetchOption.uuid)
+            state.accessToken = obj.accessToken
+            state.refreshToken = obj.refreshToekn
+            state.fetchOption.uuid = obj.fetchOption.uuid
+            state.authenticated = obj.authenticated
             return {
                 ...state,
-                accessToken: action.accessToken,
-                refreshToekn: action.refreshToken,
-                authenticated: action.result,
+                accessToken: obj.accessToken,
+                refreshToekn: obj.refreshToken,
+                authenticated: obj.authenticated,
+                fetchOption: obj.fetchOption.uuid,
             }
+        }
 
         case UserActionType.type.signout:
             UserReducer.clearUserState()
