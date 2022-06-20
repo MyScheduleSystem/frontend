e1859e4ea727a34ed96d6de84053283bce89b5cf
foxmon <jjockrod@naver.com>
2022-06-20 14:12:26 +0900

    Refactor: refreshtoken을 담는 cookie storage로 수정

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,32 +1,37 @@
+import SymbolName from '../util/symbolName'
+import Cookies from 'universal-cookie'
 import ErrorUtil from "../util/errorUtil"
 
-class Storage {
-    constructor(uuid, obj) {
-        this.uuid = uuid
-        this.obj = obj
-    }
+const cookies = new Cookies()
+const REFRESH_TOKEN = 'RefreshToken'
 
-    save() {
-        // Storage를 제대로 생성하지 않고 save만 사용하는 경우. 예외처리
-        ErrorUtil.invalidParameter(this.uuid)
-        ErrorUtil.invalidParameter(this.obj)
-        localStorage.setItem(this.uuid, JSON.stringify(this.obj))
-    }
-
-    getToken() {
-        // uuid 없이 token에 접근하려는 경우 예외처리
-        ErrorUtil.invalidParameter(this.uuid)
-        return JSON.parse(localStorage.getItem(this.uuid))
-    }
-
-    clear() {
-        localStorage.clear()
+// Refresh token => cookie
+class Storage extends SymbolName {
+    constructor() {
+        super('storage')
+        SymbolName.freezeObject(this, Storage)
     }
 }
 
-Storage.checkPersistenceUser = function(uuid) {
-    return JSON.parse(localStorage.getItem(uuid))
+// samesite option => cookie를 서로 공유 가능한가?
+// strict: 같은 도메인에서 접근 가능
+// Lax: a tag, link tag를 활용하여 이동 시 cookie 전송
+// none, cross-site에서도 쿠기 전송이 가능해짐. 단, secure속성 추가
+Storage.saveRefershToken = function(refreshToken) {
+    ErrorUtil.invalidParameter(refreshToken)
+    cookies.set(
+        REFRESH_TOKEN,
+        refreshToken,
+        { sameSite: 'strict' },
+    )
 }
 
-Object.freeze(Storage)
+Storage.checkPersistenceUser = function() {
+    return cookies.get(REFRESH_TOKEN)
+}
+
+Storage.clear = function() {
+    cookies.remove(REFRESH_TOKEN)
+}
+
 export default Storage
