68e5d5765375d7993f8a652b65bd02d06f9accc6
Velo2 <jhpark4884@gmail.com>
2022-07-05 21:17:37 +0900

    google signup 수정필요

--- src/fetcher/userFetcher.js
+++ src/fetcher/userFetcher.js
@@ -67,7 +67,13 @@ userFetcher.signupWithGoogle = async function() {
     const googleProvider = new GoogleAuthProvider()
     return signInWithPopup(auth, googleProvider)
         .then((result) => {
+            const user = {}
             const credential = GoogleAuthProvider.credentialFromResult(result)
+            // user.email = result.user.auth.email
+            console.log(credential.accessToken)
+            console.log(result.user.email)
+            console.log(result.user.uid)
+            // console.log(result.user.uid)
             return credential ? true : false
         })
         .catch(e => console.log(e))
