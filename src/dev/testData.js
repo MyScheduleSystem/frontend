41c3474908fe9be90d9572aae0c09fc217e29daa
foxmon <jjockrod@naver.com>
2022-07-09 23:13:27 +0900

    testData 수정

--- src/dev/testData.js
+++ src/dev/testData.js
@@ -19,13 +19,13 @@ TestData[userId] = {
         month: "",
         [date]: {
             calendar: {
-                [date]:[
+                [date]: [
                     {
                         title: "",
                         content: "",
                         startDate: "",
                         endDate: "",
-                    }
+                    },
                 ],
             },
         },
@@ -42,18 +42,42 @@ TestData[userId] = {
             year: "",
             month: "",
             themeMode: "",
-        }
-    }
+        },
+    },
 }
 
 function create(calendarSpecJson) {
     const testDays = [
-        "2022-06-01", "2022-06-02", "2022-06-03", "2022-06-04", "2022-06-05",
-        "2022-06-06", "2022-06-07", "2022-06-08", "2022-06-09", "2022-06-10",
-        "2022-06-11", "2022-06-12", "2022-06-13", "2022-06-14", "2022-06-15",
-        "2022-06-16", "2022-06-17", "2022-06-18", "2022-06-19", "2022-06-20",
-        "2022-06-21", "2022-06-22", "2022-06-23", "2022-06-24", "2022-06-25",
-        "2022-06-26", "2022-06-27", "2022-06-28", "2022-06-29", "2022-06-30",
+        "2022-06-01",
+        "2022-06-02",
+        "2022-06-03",
+        "2022-06-04",
+        "2022-06-05",
+        "2022-06-06",
+        "2022-06-07",
+        "2022-06-08",
+        "2022-06-09",
+        "2022-06-10",
+        "2022-06-11",
+        "2022-06-12",
+        "2022-06-13",
+        "2022-06-14",
+        "2022-06-15",
+        "2022-06-16",
+        "2022-06-17",
+        "2022-06-18",
+        "2022-06-19",
+        "2022-06-20",
+        "2022-06-21",
+        "2022-06-22",
+        "2022-06-23",
+        "2022-06-24",
+        "2022-06-25",
+        "2022-06-26",
+        "2022-06-27",
+        "2022-06-28",
+        "2022-06-29",
+        "2022-06-30",
     ]
     calendarSpecJson = {}
     testDays.forEach((e, i) => {
@@ -63,14 +87,50 @@ function create(calendarSpecJson) {
         todoObj.content = `Todo Content${i}`
         todoObj.startDate = e
         todoObj.endDate = DateUtil.dateCalculate(e, i, "add", "days")
-        todoObj.isCompleted = (i % 2 === 0) ? true : false
-        calendarSpecJson[e].push(new TodoItem(i, todoObj.title, todoObj.content, todoObj.startDate, todoObj.endDate, todoObj.isCompleted))
+        todoObj.isCompleted = i % 2 === 0 ? true : false
+        calendarSpecJson[e].push(
+            new TodoItem(
+                i,
+                todoObj.title,
+                todoObj.content,
+                todoObj.startDate,
+                todoObj.endDate,
+                todoObj.isCompleted
+            )
+        )
     })
     calendarSpecJson["2022-06-19"] = []
-    calendarSpecJson["2022-06-19"].push(new TodoItem(100, "Todo test velo", "Todo test100", "2022-06-19", "2022-06-20", false))
-    calendarSpecJson["2022-06-19"].push(new TodoItem(101, "Todo test velo", "Todo test100", "2022-06-19", "2022-06-30", false))
+    calendarSpecJson["2022-06-19"].push(
+        new TodoItem(
+            100,
+            "Todo test velo",
+            "Todo test100",
+            "2022-06-19",
+            "2022-06-20",
+            false
+        )
+    )
+    calendarSpecJson["2022-06-19"].push(
+        new TodoItem(
+            101,
+            "Todo test velo",
+            "Todo test100",
+            "2022-06-19",
+            "2022-06-30",
+            false
+        )
+    )
     calendarSpecJson["2022-06-22"] = []
-    calendarSpecJson["2022-06-22"].push(new TodoItem(102, "Todo test velo", "Todo test100", "2022-06-22", "2022-06-23", false))
+    calendarSpecJson["2022-06-22"].push(
+        new TodoItem(
+            102,
+            "Todo test velo",
+            "Todo test100",
+            "2022-06-22",
+            "2022-06-23",
+            false
+        )
+    )
     // console.log(calendarSpecJson)
     return calendarSpecJson
 }
@@ -84,8 +144,8 @@ export function createFriendsList() {
     const name = "FoxMon"
     // const createdAt = "2022-05-20"
     const arr = []
-    for(let i = 0; i < 10; i++) {
-        arr.push(new Friend(i + 1, `${name}${i + 1}`, "2022-05-20", "", `MSS!${i + 1}`))
+    for (let i = 0; i < 10; i++) {
+        arr.push(new Friend(i + 1, `${name}${i + 1}`, "", `MSS!${i + 1}`))
     }
     const fArr = FriendList.createFriendList(arr)
     return fArr
@@ -95,10 +155,18 @@ export function createChatRoomList() {
     const name = "FoxMon's Chat Room"
     const arr = []
     const users = []
-    for(let i = 0; i < 5; i++) {
-        users.push(new User(i, `FoxMon${i}`, "상메1", `FoxMon@1234${i + 100}`, `FoxMon${i + 1}`))
+    for (let i = 0; i < 5; i++) {
+        users.push(
+            new User(
+                i,
+                `FoxMon${i}`,
+                "상메1",
+                `FoxMon@1234${i + 100}`,
+                `FoxMon${i + 1}`
+            )
+        )
     }
-    for(let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
         arr.push(new ChatRoom(i + 1, `${name}${i + 1}`, users))
     }
     const cArr = ChatRoomList.createChatRoomList(arr)
@@ -107,12 +175,20 @@ export function createChatRoomList() {
 
 export function createNotify() {
     const testDays = [
-        "2022-05-12", "2022-05-13", "2022-05-14", "2022-05-15", "2022-05-16",
-        "2022-05-17", "2022-05-18", "2022-05-19", "2022-05-20", "2022-05-21",
+        "2022-05-12",
+        "2022-05-13",
+        "2022-05-14",
+        "2022-05-15",
+        "2022-05-16",
+        "2022-05-17",
+        "2022-05-18",
+        "2022-05-19",
+        "2022-05-20",
+        "2022-05-21",
     ]
     const isChecked = false
     const message = "Arrived Message!"
-    const nArr =  []
+    const nArr = []
     testDays.forEach((d, i) => {
         const obj = new Notify(i, d, isChecked, message, d)
         nArr.push(obj)
@@ -122,15 +198,29 @@ export function createNotify() {
 
 export function createMessage() {
     const testDays = [
-        "2022-05-12", "2022-05-13", "2022-05-14", "2022-05-15", "2022-05-16",
-        "2022-05-17", "2022-05-18", "2022-05-19", "2022-05-20",
+        "2022-05-12",
+        "2022-05-13",
+        "2022-05-14",
+        "2022-05-15",
+        "2022-05-16",
+        "2022-05-17",
+        "2022-05-18",
+        "2022-05-19",
+        "2022-05-20",
     ]
     const isChecked = false
     const message = "Arrived Message!"
     const friendName = "FoxMon"
-    const mArr =  []
+    const mArr = []
     testDays.forEach((d, i) => {
-        const obj = new Message(i, `${message}${i}`, `${friendName}${i}`, isChecked, d, d)
+        const obj = new Message(
+            i,
+            `${message}${i}`,
+            `${friendName}${i}`,
+            isChecked,
+            d,
+            d
+        )
         mArr.push(obj)
     })
     return mArr
@@ -142,7 +232,14 @@ export function createSnsFriendList() {
     const friendImageUrl = "/images/instargramAvatar.jpg"
     const arr = []
     for (let i = 0; i < 10; i++) {
-        arr.push(new Friend(i + 1, `${friendName}`, `${createdAt}`,`${friendImageUrl}`))
+        arr.push(
+            new Friend(
+                i + 1,
+                `${friendName}`,
+                `${createdAt}`,
+                `${friendImageUrl}`
+            )
+        )
     }
     const sArr = FriendList.createFriendList(arr)
     return sArr
