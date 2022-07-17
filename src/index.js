679f9c69962f9ea6627e0cf94132e133ee262a93
vpvm96 <vpvm96@gmail.com>
2022-07-17 17:59:47 +0900

    React Dnd 삭제

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import UserContextProvider from "./context/userContextProvider"
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
 import App from "./app"
 import "./index.css"
 
@@ -11,11 +9,9 @@ import "./index.css"
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <DndProvider backend={HTML5Backend}>
-                <UserContextProvider>
-                    <App />
-                </UserContextProvider>
-            </DndProvider>
+            <UserContextProvider>
+                <App />
+            </UserContextProvider>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
