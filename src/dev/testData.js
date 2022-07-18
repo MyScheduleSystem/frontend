33d2ce165221d015e420000fa8176b5b97e0a2f8
vpvm96 <vpvm96@gmail.com>
2022-07-18 17:43:19 +0900

    testDate ChatRoom 부분 삭제

--- src/dev/testData.js
+++ src/dev/testData.js
@@ -1,30 +1,5 @@
-import ChatRoom from "../type/chatRoom"
-import ChatRoomList from "../type/chatRoomList"
 import Friend from "../type/friend"
 import FriendList from "../type/friendList"
-import User from "../type/user"
-
-export function createChatRoomList() {
-    const name = "FoxMon's Chat Room"
-    const arr = []
-    const users = []
-    for (let i = 0; i < 5; i++) {
-        users.push(
-            new User(
-                i,
-                `FoxMon${i}`,
-                "상메1",
-                `FoxMon@1234${i + 100}`,
-                `FoxMon${i + 1}`
-            )
-        )
-    }
-    for (let i = 0; i < 10; i++) {
-        arr.push(new ChatRoom(i + 1, `${name}${i + 1}`, users))
-    }
-    const cArr = ChatRoomList.createChatRoomList(arr)
-    return cArr
-}
 
 export function createSnsFriendList() {
     const friendName = "이주빈"
