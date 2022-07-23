b47cb9d87d7d358268102e149791b2890b43e762
foxmon <jjockrod@naver.com>
2022-07-23 22:12:18 +0900

    Refactor: myChatRoomList Type 적용

--- src/components/chat/myChatRoom.jsx
+++ src/components/chat/myChatRoom.jsx
@@ -2,14 +2,10 @@ import React, { useState, useEffect } from "react"
 import MyChatRoomInput from "./myChatRoomInput"
 import MyChatRoomUserList from "./myChatRoomUserList"
 import MyChatRoomMessage from "./myChatRoomMessage"
+import User from "../../type/user"
 import userFetcher from "../../fetcher/userFetcher"
 import { useLocation } from "react-router"
-import {
-    Container,
-    Grid,
-    Paper,
-    Divider,
-} from "@mui/material"
+import { Container, Grid, Paper, Divider } from "@mui/material"
 
 const MyChatRoom = () => {
     const [myInfo, setMyInfo] = useState({})
@@ -19,21 +15,23 @@ const MyChatRoom = () => {
 
     useEffect(() => {
         doFetchMyInformation.call(this, state.uuid, setMyInfo)
-        doFetchFriendInformation.call(this, state.friends, state.uuid, setFriendInfo)
+        doFetchFriendInformation.call(
+            this,
+            state.friends,
+            state.uuid,
+            setFriendInfo
+        )
     }, [])
 
     return (
         <React.Fragment>
             <Container sx={containerStyle}>
                 <Grid container={true}>
-                    <Grid
-                        container={true}
-                        component={Paper}
-                    >
-                        <MyChatRoomUserList 
-                            chatRoomName={state.chatRoomName} 
-                            myInfo={myInfo} 
-                            friendInfo={friendInfo} 
+                    <Grid container={true} component={Paper}>
+                        <MyChatRoomUserList
+                            chatRoomName={state.chatRoomName}
+                            myInfo={myInfo}
+                            friendInfo={friendInfo}
                         />
                         <Grid item={true} xs={9}>
                             <MyChatRoomMessage />
@@ -57,23 +55,32 @@ const containerStyle = {
 export default MyChatRoom
 
 function doFetchMyInformation(uuid, setMyInfo) {
-    userFetcher.getMyInformationByUuid(uuid)
-            .then((result) => {
-                result.forEach((d) => {
-                    setMyInfo(d.data())
-                })
-            })
+    userFetcher.getMyInformationByUuid(uuid).then((result) => {
+        result.forEach((d) => {
+            const data = d.data()
+            const obj = {}
+            obj.uuid = data.uuid
+            obj.name = data.name
+            obj.username = data.username
+            obj.email = data.email
+            obj.profileUrl = data.profileUrl
+            obj.infoMessage = data.infoMessage
+            const uObj = new User(obj.uuid, obj.username, obj.email, obj.name)
+            uObj.info.setProfileUrl(obj.profileUrl)
+            uObj.info.setInfoMessage(obj.infoMessage)
+            setMyInfo(uObj)
+        })
+    })
 }
 
 function doFetchFriendInformation(friends, uuid, setFriendInfo) {
     friends.forEach((u) => {
-        if(u !== uuid) {
-            userFetcher.getMyInformationByUuid(u)
-                .then((result) => {
-                    result.forEach((d) => {
-                        setFriendInfo((prev) => [ ...prev, d.data() ])
-                    })
+        if (u !== uuid) {
+            userFetcher.getMyInformationByUuid(u).then((result) => {
+                result.forEach((d) => {
+                    setFriendInfo((prev) => [...prev, d.data()])
                 })
+            })
         }
     })
 }
